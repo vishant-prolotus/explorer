226a89db99d1e5223377b0e0a65a8f2b06084aae
vishant-prolotus <vishanteotia@gmail.com>
2018-07-25 16:30:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -40,6 +40,10 @@ module.exports = {
 
     TFC: "http://explorer.freedom-coin.io",
 
-    currencies: [{ "name": "HLD" }, { "name": "BTCI" }, { "name": "LPC" }, { "name": "RVN" }, { "name": "C2C" }, { "name": "DVX" }, { "name": "KONA" }, { "name": "VYI" }, { "name": "POLIS" }, { "name": "BWS" }, { "name": "CPRA" }, { "name": "ION" }, { "name": "SAGA" }, { "name": "LINC" }, { "name": "CARE" }, { "name": "HARC" }, { "name": "TFC" }]
+    currencies: [{ "name": "HLD" }, { "name": "BTCI" }, { "name": "LPC" }, { "name": "RVN" },
+                // { "name": "C2C" }, { "name": "DVX" }, { "name": "KONA" }, { "name": "VYI" }, 
+                 //{ "name": "POLIS" }, { "name": "BWS" }, { "name": "CPRA" }, { "name": "ION" }, 
+                // { "name": "SAGA" }, { "name": "LINC" }, { "name": "CARE" }, { "name": "HARC" },
+                 { "name": "TFC" }]
 
 }
